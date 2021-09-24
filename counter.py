07570099a103ce5e8f404dd6586f6acc8ed25499
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2021-09-24 21:25:25 +0800

    Update counter.py

--- counter.py
+++ counter.py
@@ -8,7 +8,7 @@ def people_counter(image):
     open_cv_image = np.array(image) 
     # Convert RGB to BGR 
     image = open_cv_image[:, :, ::-1].copy() 
-    face_cascade = cv2.CascadeClassifier('./assets/haarcascade_frontalface_alt.xml')
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     #print (len(faces))
