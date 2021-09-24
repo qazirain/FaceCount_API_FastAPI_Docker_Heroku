0048062edc1b8fb6182e558c9e59546f57dd3667
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2021-09-24 19:41:47 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from .components.counter import people_counter, read_imagefile
+
+app = FastAPI()
+
+@app.post("/predict/image")
+async def predict_api(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png", "JPG", "PNG", "JPEG")
+    if not extension:
+        return "Image must be jpg or png format!"
+    image = read_imagefile(await file.read())
+    prediction = people_counter(image)
+    
+    return prediction
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
